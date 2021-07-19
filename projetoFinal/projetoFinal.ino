a4c4e7f4adff35511c8c7415a8984bc43bf739b7
João Victor <54864686+victor-souza1997@users.noreply.github.com>
2021-07-19 15:34:41 -0300

    baudrated updated to better visualization of the incoming signal

--- projetoFinal/projetoFinal.ino
+++ projetoFinal/projetoFinal.ino
@@ -6,11 +6,12 @@ int input;//variavel que armazenara a entrada de audio
 
 void setup() {
   pinMode(Pin35, input);
-  Serial.begin(19200);
+  Serial.begin(115200);
 }
 
 void loop() {
   input = analogRead(Pin35);
+  float s = input*3.3/4096;
   Serial.println(input);
   // put your main code here, to run repeatedly:
 
