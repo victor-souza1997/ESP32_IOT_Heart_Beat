db65643e97602262dc1bab177cb4b4bde24e2a8c
João Victor <54864686+victor-souza1997@users.noreply.github.com>
2021-07-20 13:15:14 -0300

    function interrupt added

--- projetoFinal/projetoFinal.ino
+++ projetoFinal/projetoFinal.ino
@@ -2,17 +2,44 @@
 
 #define Pin35 35
 
-int input;//variavel que armazenara a entrada de audio
+#define amos_per_sec 2000 //quantidade de amostras coletadas por segundo
 
-void setup() {
-  pinMode(Pin35, input);
-  Serial.begin(115200);
+int input;//variavel que armazenara a entrada de audio
+volatile int interruptCounter;
+int totalInterruptCounter;
+ 
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+ 
+void IRAM_ATTR onTimer() { //funcao de interrupt
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+ 
 }
 
-void loop() {
-  input = analogRead(Pin35);
-  float s = input*3.3/4096;
-  Serial.println(s);
-  // put your main code here, to run repeatedly:
+void setup() { //funcao de configuracao
+  pinMode(Pin35, input);//configurando pino de amotragem para o pino Pin35
+  Serial.begin(115200);//iniciando comunicao serial
+  
+  //inicializando timer para interrupt
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000/amos_per_sec, true);
+  timerAlarmEnable(timer);
+}
+
+void loop() {//funcao loop
+   if (interruptCounter > 0){//caso o contador de interrupt seja maior que 0
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+    totalInterruptCounter++;
+    //Serial.print("An interrupt as occurred. Total number: ");
+    //Serial.println(totalInterruptCounter);
+    input = analogRead(Pin35);
+    float s = input*3.3/4096;
+    Serial.println(s);
+  }
 
 }
